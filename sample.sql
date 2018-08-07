b58613310dc056c2bf63b760f06587d039961a6a
Meghanakanakavidu <meghana.kanakavidu@gmail.com>
2018-08-07 18:40:30 +0530

    database

---
+++ sample.sql
@@ -0,0 +1,3 @@
+create database sample;
+use sample
+CREATE TABLE USER (first_name VARCHAR(100), last_name VARCHAR(100),email VARCHAR(100), username VARCHAR(100), password VARCHAR(100), regdate  VARCHAR(100));
