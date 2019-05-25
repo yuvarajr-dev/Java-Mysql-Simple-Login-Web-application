5cc3310c51445c44a5e7866a583766bc783f3057
root <root@meghjenkins.fwcnccvptddejchq1mljlyvkzh.gx.internal.cloudapp.net>
2019-05-25 12:44:40 +0000

    final

--- vars/sharedlib.groovy
+++ vars/sharedlib.groovy
@@ -1,46 +1,47 @@
-def call(Map params) { 
+def call(Map params) {
+try{
 node('master') {
     stage('pull') {
         checkout([$class: 'GitSCM', branches: [[name: params.branch ]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: params.giturl ]]])
     }
     stage('artifact details') {
         tool name: 'java', type: 'jdk'
-	    tool name: 'maven', type: 'maven'
-	    def mvnHome = tool 'maven'
-	    env.PATH = "${mvnHome}/bin:${env.PATH}"
+            tool name: 'maven', type: 'maven'
+            def mvnHome = tool 'maven'
+            env.PATH = "${mvnHome}/bin:${env.PATH}"
         rtMavenDeployer (
           id: 'deployer-unique-id',
           serverId: params.artifactid,
           releaseRepo: 'release/${BUILD_NUMBER}',
           snapshotRepo: "snapshot/${BUILD_NUMBER}"
         )
-        
+
     }
     stage('maven build'){
-        
+
           rtMavenRun (
           tool: 'maven',
-	  type: 'maven',
+          type: 'maven',
           pom: 'pom.xml',
           goals: 'clean install',
           opts: '-Xms1024m -Xmx4096m',
           //resolverId: 'resolver-unique-id',
           deployerId: 'deployer-unique-id',
           )
-        
+
     }
     stage('publish the artifact'){
-        
+
           rtPublishBuildInfo (
           serverId: params.artifactid
           )
-         
+
     }
     stage('sonar'){
         // def scannerHome = tool 'Sonar';
         withSonarQubeEnv(params.sonarid) {
          sh 'mvn clean install sonar:sonar'
-       }  
+       }
     }
      stage("Quality Gate"){
       timeout(time: 60, unit: 'SECONDS') { // Just in case something goes wrong, pipeline will be killed after a timeout
@@ -49,7 +50,7 @@ node('master') {
         error "Pipeline aborted due to quality gate failure: ${qg.status}"
     }
   }
-  
+
  }
     stage('Download artifact'){
         rtDownload (
@@ -67,17 +68,17 @@ node('master') {
      }
     stage('Copy'){
         sh 'mv /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/*.war  /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/loginwebapp.war'
-         
-        sh 'scp -v -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/*.war root@'+params.destip+':tomcat/webapps'    
 
-        
+        sh 'scp -v -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/*.war root@'+params.destip+':tomcat/webapps'
+
+
     }
-    }    
+    }
+	}
     catch (err) {
       mail body:"${err}", subject: 'Build Failed', to: params.email
       currentBuild.result = 'FAILURE'
       }
 
 }
-
 
