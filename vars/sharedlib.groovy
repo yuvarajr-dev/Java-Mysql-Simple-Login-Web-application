cc1bec57c67da16279072ae950142680b557724d
root <root@meghjenkins.fwcnccvptddejchq1mljlyvkzh.gx.internal.cloudapp.net>
2019-05-26 05:06:30 +0000

    final

--- vars/sharedlib.groovy
+++ vars/sharedlib.groovy
@@ -70,11 +70,12 @@ node('master') {
         sh 'mv /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/*.war  /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/loginwebapp.war'
 
         sh 'scp -v -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/*.war root@'+params.destip+':tomcat/webapps'
+		sh 'scp -v -o StrictHostKeyChecking=no  /var/lib/jenkins/workspace/${JOB_NAME}/${BUILD_NUMBER}/com/javawebtutor/LoginWebApp/1.0-SNAPSHOT/*.war root@'+params.nginxip':tomcat/webapps'
 
 
     }
     }
-	}
+        }
     catch (err) {
       mail body:"${err}", subject: 'Build Failed', to: params.email
       currentBuild.result = 'FAILURE'
